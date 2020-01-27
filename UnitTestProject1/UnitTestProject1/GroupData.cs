84323169d4688ffcaaaa83b2c77887082373498e
jane <jane.shysh@gmail.com>
2020-01-27 11:48:51 -0500

    adding paratemers to Group Creation tests

---
+++ UnitTestProject1/UnitTestProject1/GroupData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+        private string groupName;
+        private string groupHeader;
+        private string groupFooter;
+
+        public GroupData(string groupName)
+        {
+            this.groupName = groupName;
+        }
+
+        public string GroupName
+        {
+            get
+            {
+                return groupName;
+            }
+            set
+            {
+                groupName = value;
+            }
+        }
+
+        public string GroupHeader
+        {
+            get
+            {
+                return groupHeader;
+            }
+            set
+            {
+                groupHeader = value;
+            }
+        }
+        public string GroupFooter
+        {
+            get
+            {
+                return groupFooter;
+            }
+            set
+            {
+                groupFooter = value;
+            }
+        }
+    }
+}
--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using addressbook_web_tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WebAddressbookTests
@@ -9,6 +10,38 @@ namespace WebAddressbookTests
         [TestMethod]
         public void TestMethod1()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Circle c1 = new Circle(5);
+            Circle c2 = new Circle(15);
+            Circle c3 = c1;
+
+            Assert.AreEqual(c1.Radius, 5);
+            Assert.AreEqual(c2.Radius, 10);
+            Assert.AreEqual(c3.Radius, 5);
+
+            c3.Radius = 15;
+
+            Assert.AreEqual(c1.Radius, 15);
+
         }
     }
 }
