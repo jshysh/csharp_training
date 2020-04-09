15b2bb7ebf8ac7d19e52d2846dbb44b68069ef3a
jane <jane.shysh@gmail.com>
2020-04-09 13:56:44 -0400

    Finished Lesson6.2 for Groups

--- UnitTestProject1/UnitTestProject1/tests/GroupCreationTests.cs
+++ UnitTestProject1/UnitTestProject1/tests/GroupCreationTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace WebAddressbookTests
 {
@@ -22,7 +24,7 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        public static IEnumerable<GroupData> GroupDataFromFile()
+        public static IEnumerable<GroupData> GroupDataFromCsvFile()
         {
             List<GroupData> groups = new List<GroupData>();
             string[] lines = File.ReadAllLines(@"groups.csv");
@@ -38,7 +40,16 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("GroupDataFromFile")]
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            return (List<GroupData>) 
+                new XmlSerializer(typeof(List<GroupData>))
+                    .Deserialize(new StreamReader(@"groups.xml"));
+            
+        }
+
+        [Test, TestCaseSource("GroupDataFromXmlFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
@@ -66,5 +77,4 @@ namespace WebAddressbookTests
             List<GroupData> newGroups = app.Groups.GetGroupList();
         }
     }
-
 }
--- UnitTestProject1/addressbook-test-data-generator/Program.cs
+++ UnitTestProject1/addressbook-test-data-generator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,16 +16,43 @@ namespace addressbook_test_data_generator
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[2];
 
+            List<GroupData> groups = new List<GroupData>();
             for (int i = 0; i < count; i++)
             {
-                writer.WriteLine(String.Format("${0},${1},${2}",
-                    TestBase.GenerateRandomString(10),
-                    TestBase.GenerateRandomString(10),
-                    TestBase.GenerateRandomString(10)));
+                groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                {
+                    Header = TestBase.GenerateRandomString(10),
+                    Footer = TestBase.GenerateRandomString(10)
+                });
+            }
+            if (format == "csv")
+            {
+                writeGroupsToCsvFile(groups, writer);
+            } else if (format == "xml")
+            {
+                writeGroupsToXmlFile(groups, writer);
+            } else
+            {
+                System.Console.Out.Write("Unrecognized format" + format);
             }
             writer.Close();
-            TestBase.GenerateRandomString(10);
+        }
+
+        static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach (GroupData group in groups)
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",
+                    group.Name, group.Header, group.Footer
+                    ));
+            }
+        }
+
+        static void writeGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
         }
     }
 }
